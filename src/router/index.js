99595a422d9252ce585bc60fb3baee02b15f8e9c
lius <523595992@qq.com>
2018-07-09 15:19:35 +0800

    修改文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
     }
   ]
 })
